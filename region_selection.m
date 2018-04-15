8ae905376907cf4b1c562e2f76b05440a735d27a
Rose Rustowicz <roserustowicz@DN0a1ea715.SUNet>
2018-04-14 22:05:09 -0700

    Adding labeled image clips

--- region_selection.m
+++ region_selection.m
@@ -3,7 +3,7 @@ size_of_region = 32;
 regions = zeros(size_of_region,size_of_region,1);
 
 % loading image
-img = im2double(imread('data/ks_2_30.png'));
+img = im2double(imread('data/saudi_2013_30.png'));
 plt = figure();
 imshow(img);
 title(sprintf('press "return" to quit,\n press/click anything else to continue,\n if region selected is greater than %dX%d it is not stored.',size_of_region,size_of_region))
@@ -52,4 +52,4 @@ while(strcmp(get(gcf,'CurrentKey'),'return')~= 1)
     waitforbuttonpress
 end
 % saving regions 3D matrix
-save('ks_2_30_01.mat','regions')
+save('saudi_2013_clips.mat','regions')
